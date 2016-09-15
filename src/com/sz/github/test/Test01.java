13aab7ce2944b8b83d68bbcbb91fe604b9d30c2d
shizhuo09 <1695719353@qq.com>
2016-09-15 14:26:06 +0800

    last edit
    last edit

--- src/com/sz/github/test/Test01.java
+++ src/com/sz/github/test/Test01.java
@@ -6,5 +6,6 @@ public class Test01 {
 		System.out.println("hello,github服务器");
 		System.out.println("ok,github服务器,return 01");
 		System.out.println("last modify");
+		System.out.println("last Edit");
 	}
 }
