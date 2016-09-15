177b344de604754f7c8f84b69e34df3fe4792cba
shizhuo09 <1695719353@qq.com>
2016-09-15 14:23:11 +0800

    测试pull
    测试pull

--- src/com/sz/github/test/Test01.java
+++ src/com/sz/github/test/Test01.java
@@ -5,5 +5,6 @@ public class Test01 {
 	public static void main(String[] args) {
 		System.out.println("hello,github服务器");
 		System.out.println("ok,github服务器,return 01");
+		System.out.println("last modify");
 	}
 }
