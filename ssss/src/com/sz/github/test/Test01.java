431441e61ca686dad6ac49ddaea5c4e88b3ad33c
shizhuo09 <1695719353@qq.com>
2016-09-22 09:40:26 +0800

    dssssss

---
+++ ssss/src/com/sz/github/test/Test01.java
@@ -0,0 +1,11 @@
+package com.sz.github.test;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+		System.out.println("hello,github服务器");
+		System.out.println("ok,github服务器,return 01");
+		System.out.println("last modify");
+		System.out.println("last Edit");
+	}
+}
